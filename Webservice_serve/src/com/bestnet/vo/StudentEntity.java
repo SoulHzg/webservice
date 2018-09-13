ac876bf0a9a237a981a930b644df3b22f05a0357
soul <845948188@qq.com>
2018-09-13 18:01:20 +0800

    spring+hibernate+webservice

---
+++ Webservice_serve/src/com/bestnet/vo/StudentEntity.java
@@ -0,0 +1,63 @@
+package com.bestnet.vo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student", schema = "spring_hibernate", catalog = "")
+public class StudentEntity {
+    private String id;
+    private String name;
+    private String sex;
+
+    @Basic
+    @Column(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "sex")
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StudentEntity that = (StudentEntity) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (sex != null ? !sex.equals(that.sex) : that.sex != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (sex != null ? sex.hashCode() : 0);
+        return result;
+    }
+}
